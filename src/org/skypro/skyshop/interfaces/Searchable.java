c17d0fa049dcf2c28ef1381ff4ef2c38a66af3ba
SiniWire <ppm2004maksim@gmail.com>
2026-07-10 22:53:36 +0300

    Quadruple commit!

---
+++ src/org/skypro/skyshop/interfaces/Searchable.java
@@ -0,0 +1,11 @@
+package org.skypro.skyshop.interfaces;
+
+public interface Searchable {
+    String getSearchTerm();
+    String getContentType();
+    String getName();
+
+    default String getStringRepresentation() {
+        return getName() + " — " + getContentType();
+    }
+}
